0d2f165e519ba9249464b8339550bc9d04c39a7d
fsl4faisal <fsl4faisal@gmail.com>
2023-01-13 08:26:45 +0530

    adding some files

---
+++ JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.jupiter.api.Test;
+
+class StringHelperTest {
+
+    @Test
+    void truncateAInFirst2Positions() {
+    }
+
+    @Test
+    void areFirstAndLastTwoCharactersTheSame() {
+    }
+}
